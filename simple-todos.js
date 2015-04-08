83e88d1c84a5877968b6ff860b21bad2862b3605
Carol St.Louis <stlouis_c@yahoo.com>
2015-04-08 02:20:36 -0700

    added option of hiding completed tasks -- checkmark (simple-todos.html) and event handler (monitor change of hide-completed input) and helper to set session information & save preference for duration of current session (simple-todos.js) -- and added remaining todos counter (html & helper in js)

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,17 @@ if (Meteor.isClient) {
   // This code only runs in the client
   Template.body.helpers({
     tasks: function(){
-      return Tasks.find({}, {sort:{createdAt: -1}});
+      if(Session.get('hideCompleted')){
+        return Tasks.find({checked: {$ne: true}}, {sort:{createdAt: -1}});        
+      }else{
+        return Tasks.find({}, {sort:{createdAt: -1}});
+      }
+    },
+    hideCompleted: function(){
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function(){
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
   Template.body.events({
@@ -19,6 +29,9 @@ if (Meteor.isClient) {
       event.target.text.value="";
       //prevent default form submit
       return false;
+    },
+    "change .hide-completed input": function(event){
+      Session.set("hideCompleted", event.target.checked);
     }
   });
   Template.task.events({
