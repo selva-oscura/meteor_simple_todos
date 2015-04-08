0a1c9e7a9027a778106a40156694733174b0722e
Carol St.Louis <stlouis_c@yahoo.com>
2015-04-08 00:59:49 -0700

    added new-task form (simple-todos.html), added new-task event handler to insert new task into database and updated task event helper to list tasks in reverse chronological order (simple-todos.js), changed font color and new-task form background-color (simple-todos.js)

--- simple-todos.js
+++ simple-todos.js
@@ -4,9 +4,23 @@ if (Meteor.isClient) {
   // This code only runs in the client
   Template.body.helpers({
     tasks: function(){
-      return Tasks.find({});
+      return Tasks.find({}, {sort:{createdAt: -1}});
     }
   });
+  Template.body.events({
+    "submit .new-task": function(event){
+      // this funciton is called when the new-task form is submitted
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+      //clear form
+      event.target.text.value="";
+      //prevent default form submit
+      return false;
+    }
+  })
 }
 
 if (Meteor.isServer) {
