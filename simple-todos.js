df19efc20f66fc08684c5a2cc9fa348425e844e2
Carol St.Louis <stlouis_c@yahoo.com>
2015-04-07 23:59:57 -0700

    basic set-up of html, js, css files for todo project

---
+++ simple-todos.js
@@ -0,0 +1,16 @@
+if (Meteor.isClient) {
+  // This code only runs in the client
+  Template.body.helpers({
+    tasks:[
+      {text: 'This is task 1'},
+      {text: 'This is task 2'},
+      {text: 'This is task 3'}
+    ]
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
