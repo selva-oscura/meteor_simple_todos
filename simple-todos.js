81f56d4bfecdb39655dba20f46ab9674a268193d
Carol St.Louis <stlouis_c@yahoo.com>
2015-04-09 00:14:48 -0700

    added user accounts (accounts-ui, accounts-password) with log-in/sign-up, display of new-tasks form only for logged-in users, submission of new-task with user _id and username

--- simple-todos.js
+++ simple-todos.js
@@ -23,7 +23,9 @@ if (Meteor.isClient) {
       var text = event.target.text.value;
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(),              // current time
+        owner: Meteor.userId(),             // _id of logged-in user
+        username: Meteor.user().username    // username of logged-in user
       });
       //clear form
       event.target.text.value="";
@@ -42,6 +44,9 @@ if (Meteor.isClient) {
     "click .delete": function(){
       Tasks.remove(this._id);
     }
+  });
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
 
